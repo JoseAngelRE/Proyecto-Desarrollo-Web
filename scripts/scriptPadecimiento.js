0e3e8474a40690405161dcbf576652e6e31a3649
José Fernando Chan Dzib <56355924+Jfeerchan23@users.noreply.github.com>
2021-10-27 01:44:39 -0500

    Creación de fichas.
    Se crearon las fichas informativas de los padecimientos.

---
+++ scripts/scriptPadecimiento.js
@@ -0,0 +1,54 @@
+function desplegarObesidad(){
+    document.getElementById("fichaObesidad").style.display='block';
+    document.getElementById("fichaHipertension").style.display='none';
+    document.getElementById("fichaAnemia").style.display='none';
+    document.getElementById("fichaGota").style.display='none';
+    document.getElementById("fichaDiabetes").style.display='none';
+    document.getElementById("fichaCardio").style.display='none';
+
+}
+function desplegarHipertension(){
+    document.getElementById("fichaObesidad").style.display='none';
+    document.getElementById("fichaHipertension").style.display='block';
+    document.getElementById("fichaAnemia").style.display='none';
+    document.getElementById("fichaGota").style.display='none';
+    document.getElementById("fichaDiabetes").style.display='none';
+    document.getElementById("fichaCardio").style.display='none';
+    
+}
+function desplegarAnemia(){
+    document.getElementById("fichaObesidad").style.display='none';
+    document.getElementById("fichaHipertension").style.display='none';
+    document.getElementById("fichaAnemia").style.display='block';
+    document.getElementById("fichaGota").style.display='none';
+    document.getElementById("fichaDiabetes").style.display='none';
+    document.getElementById("fichaCardio").style.display='none';
+    
+}
+function desplegarGota(){
+    document.getElementById("fichaObesidad").style.display='none';
+    document.getElementById("fichaHipertension").style.display='none';
+    document.getElementById("fichaAnemia").style.display='none';
+    document.getElementById("fichaGota").style.display='block';
+    document.getElementById("fichaDiabetes").style.display='none';
+    document.getElementById("fichaCardio").style.display='none';
+    
+}
+function desplegarDiabetes(){
+    document.getElementById("fichaObesidad").style.display='none';
+    document.getElementById("fichaHipertension").style.display='none';
+    document.getElementById("fichaAnemia").style.display='none';
+    document.getElementById("fichaGota").style.display='none';
+    document.getElementById("fichaDiabetes").style.display='block';
+    document.getElementById("fichaCardio").style.display='none';
+    
+}
+function desplegarCardio(){
+    document.getElementById("fichaObesidad").style.display='none';
+    document.getElementById("fichaHipertension").style.display='none';
+    document.getElementById("fichaAnemia").style.display='none';
+    document.getElementById("fichaGota").style.display='none';
+    document.getElementById("fichaDiabetes").style.display='none';
+    document.getElementById("fichaCardio").style.display='block';
+    
+}
