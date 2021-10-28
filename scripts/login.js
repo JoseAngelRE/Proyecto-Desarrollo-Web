6842bad19d85783a00f2a963bd4fa748e60b4bf2
AndreiTorres <andreitorres09@outlook.com>
2021-10-28 16:32:49 -0500

    Haciendo validaciones basicas de los campos

--- scripts/login.js
+++ scripts/login.js
@@ -25,44 +25,86 @@ document.querySelector("#linkLogin a").onclick = () => {
   document.getElementById("login").checked = true;
 };
 
+const formLogin = document.getElementById("iniciarSesion");
+const formRegistrar = document.getElementById("registrar");
+const emailRegistro = document.getElementById("emailRegistro");
+const passwordRegistro = document.getElementById("passwordRegistro");
+const username = document.getElementById("nombreUsuario");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2Registro");
 
-const formLogin = document.getElementById('iniciarSesion');
-const formRegistrar = document.getElementById('registrar');
-const emailRegistro = document.getElementById('emailRegistro');
-const passwordRegistro = document.getElementById('passwordRegistro');
-const username = document.getElementById('nombreUsuario');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-
-// Show input error message
-function showError(input, message) {
-    const formControl = input.parentElement;
-    formControl.className = 'campo-formulario error';
-    const small = formControl.querySelector('small');
-    small.innerText = message;
+function mostrarError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "campo-formulario error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
 }
 
-// Show success outline
-function showSuccess(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'campo-formulario success';
+function mostrarExito(input) {
+  const formControl = input.parentElement;
+  formControl.className = "campo-formulario success";
 }
 
+function checarRequeridos(array) {
+  array.forEach((input) => {
+    if (input.value.trim() === "") {
+      mostrarError(input, `Este campo es requerido`);
+    } else {
+      mostrarExito(input);
+    }
+  });
+}
 
+function checarLongitud(input, min, max) {
+  if (input.value.length < min) {
+    mostrarError(input, `Este campo debe tener al menos ${min} caracteres`);
+  } else if (input.value.length > max) {
+    mostrarError(input, `Este campo debe tener maximo ${max} caracteres`);
+  } else {
+    mostrarExito(input);
+  }
+}
 
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
-//TODO Validar los datos y mostrar mensajes de error, o si fue exitoso el registro o login
-formLogin.addEventListener('submit', function(e) {
-    e.preventDefault();
-    console.log("Submiting login data");
-    console.log(email.value);
-    console.log(password.value);
+function passwordsCoinciden(input1, input2) {
+  if (input1.value !== input2.value) {
+    mostrarError(input2, "Las contraseñas no coinciden");
+  }
+}
+
+function emailValido(input) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    mostrarExito(input);
+  } else {
+    mostrarError(input, "Correo electronico no valido");
+  }
+}
+
+formLogin.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("Submiting login data");
+  checarRequeridos([email, password]);
+  emailValido(email);
+  checarLongitud(password, 8, 20);
+  console.log(email.value);
+  console.log(password.value);
 });
 
-formRegistrar.addEventListener('submit', function(e) {
-    e.preventDefault();
-    console.log("Submiting register data");
-    console.log(username.value);
-    console.log(emailRegistro.value);
-    console.log(passwordRegistro.value);
+formRegistrar.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("Submiting register data");
+  checarRequeridos([username, emailRegistro, passwordRegistro, password2]);
+  checarLongitud(username, 3, 15);
+  emailValido(emailRegistro);
+  checarLongitud(passwordRegistro, 6, 25);
+  passwordsCoinciden(passwordRegistro, password2);
+  console.log(emailRegistro.value);
+  console.log(passwordRegistro.value);
+  console.log(password2.value);
 });
