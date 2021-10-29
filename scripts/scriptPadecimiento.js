ed62420104560336edc1696c8fc9c9e2fe86b6a7
José Fernando Chan Dzib <56355924+Jfeerchan23@users.noreply.github.com>
2021-10-28 21:28:35 -0500

    Estilo padecimientos.
    Se  modificó el estilo de la página de padecimientos.

--- scripts/scriptPadecimiento.js
+++ scripts/scriptPadecimiento.js
@@ -1,10 +1,12 @@
-function desplegarObesidad(){
+function desplegarObesidad(valor){
     document.getElementById("fichaObesidad").style.display='block';
     document.getElementById("fichaHipertension").style.display='none';
     document.getElementById("fichaAnemia").style.display='none';
     document.getElementById("fichaGota").style.display='none';
     document.getElementById("fichaDiabetes").style.display='none';
     document.getElementById("fichaCardio").style.display='none';
+    limpiar();
+    document.getElementById("1").style.backgroundColor="yellow";
 
 }
 function desplegarHipertension(){
@@ -14,6 +16,9 @@ function desplegarHipertension(){
     document.getElementById("fichaGota").style.display='none';
     document.getElementById("fichaDiabetes").style.display='none';
     document.getElementById("fichaCardio").style.display='none';
+
+    limpiar();
+    document.getElementById("3").style.backgroundColor="yellow";
     
 }
 function desplegarAnemia(){
@@ -23,7 +28,8 @@ function desplegarAnemia(){
     document.getElementById("fichaGota").style.display='none';
     document.getElementById("fichaDiabetes").style.display='none';
     document.getElementById("fichaCardio").style.display='none';
-    
+    limpiar();
+    document.getElementById("6").style.backgroundColor="yellow";
 }
 function desplegarGota(){
     document.getElementById("fichaObesidad").style.display='none';
@@ -32,6 +38,9 @@ function desplegarGota(){
     document.getElementById("fichaGota").style.display='block';
     document.getElementById("fichaDiabetes").style.display='none';
     document.getElementById("fichaCardio").style.display='none';
+
+    limpiar();
+    document.getElementById("2").style.backgroundColor="yellow";
     
 }
 function desplegarDiabetes(){
@@ -41,6 +50,8 @@ function desplegarDiabetes(){
     document.getElementById("fichaGota").style.display='none';
     document.getElementById("fichaDiabetes").style.display='block';
     document.getElementById("fichaCardio").style.display='none';
+    limpiar();
+    document.getElementById("4").style.backgroundColor="yellow";
     
 }
 function desplegarCardio(){
@@ -50,5 +61,22 @@ function desplegarCardio(){
     document.getElementById("fichaGota").style.display='none';
     document.getElementById("fichaDiabetes").style.display='none';
     document.getElementById("fichaCardio").style.display='block';
+
+
+    limpiar();
+    document.getElementById("5").style.backgroundColor="yellow";
     
+}
+
+function limpiar(){
+   var botones=document.getElementById("flex-container").childNodes;
+
+   for (const boton of botones) {
+       console.log(boton)
+       if(boton.nodeType==1){
+       boton.style.backgroundColor="white";
+       }
+  
+       
+   }
 }
